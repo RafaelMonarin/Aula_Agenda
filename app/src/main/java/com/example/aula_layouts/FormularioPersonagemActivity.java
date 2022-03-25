7f3f758b010b2713f00a3ca1f59471284e51acb3
Aluno <aluno@LI.LOCAL>
2022-03-25 20:48:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula_layouts/FormularioPersonagemActivity.java
@@ -0,0 +1,14 @@
+package com.example.aula_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+    }
+}
---
+++ app/src/main/java/com/example/aula_layouts/ListaPersonagemActivity.java
@@ -0,0 +1,13 @@
+package com.example.aula_layouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListaPersonagemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+    }
+}
