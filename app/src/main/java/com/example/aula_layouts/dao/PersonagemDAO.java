db3758d5cd68cf79836a55430af1920b4692643d
RafaelMonarin <69466655+RafaelMonarin@users.noreply.github.com>
2022-04-28 17:10:17 -0300

    Comentários

--- app/src/main/java/com/example/aula_layouts/dao/PersonagemDAO.java
+++ app/src/main/java/com/example/aula_layouts/dao/PersonagemDAO.java
@@ -7,17 +7,21 @@ import java.util.List;
 
 public class PersonagemDAO {
 
+    // Declaração de variáveis.
     private final static List<Personagem> personagens = new ArrayList<>();
     private static int contadorDeIds = 1;
 
+    // Método para salvar na classe Personagem passando id e todos os dados do personagem.
     public void salva(Personagem personagemSalvo){
         personagemSalvo.setId(contadorDeIds);
         personagens.add(personagemSalvo);
         atualizaId();
     }
 
+    //+1 para o contador.
     private void atualizaId() { contadorDeIds++;}
-
+    
+    // Método para editar um personagem já criado na classe Personagem.
     public void edita(Personagem personagem){
         Personagem personagemEncontrado = buscaPersonagemId(personagem);
         if(personagemEncontrado != null){
@@ -26,6 +30,7 @@ public class PersonagemDAO {
         }
     }
 
+    //Busca o personagem pelo id.
     private Personagem buscaPersonagemId(Personagem personagem){
         for (Personagem p : personagens){
             if(p.getId() == personagem.getId()){
@@ -35,8 +40,10 @@ public class PersonagemDAO {
         return null;
     }
 
+    //Cria uma nova lista da classe Personagem e retorna a classe com os personagens editados.
     public List<Personagem> todos() {return new ArrayList<>(personagens);}
 
+    //Método para remover um personagem na classe Personagem.
     public void remove(Personagem personagem){
         Personagem personagemDevolvido = buscaPersonagemId(personagem);
         if(personagemDevolvido != null){
