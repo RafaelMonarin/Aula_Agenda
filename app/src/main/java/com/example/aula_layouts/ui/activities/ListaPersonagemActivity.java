60f1caf6b4e8f2cb2379b5751dd4d87fc9dd08b6
RafaelMonarin <69466655+RafaelMonarin@users.noreply.github.com>
2022-04-28 20:41:56 -0300

    Update ListaPersonagemActivity.java

--- app/src/main/java/com/example/aula_layouts/ui/activities/ListaPersonagemActivity.java
+++ app/src/main/java/com/example/aula_layouts/ui/activities/ListaPersonagemActivity.java
@@ -24,10 +24,12 @@ import static com.example.aula_layouts.ui.activities.ConstatesActivities.CHAVE_P
 
 public class ListaPersonagemActivity extends AppCompatActivity {
 
+    // Declaração das variáveis.
     public static final String TITULO_APPBAR = "Lista de Personagens";
     private final PersonagemDAO dao = new PersonagemDAO();
     private ArrayAdapter<Personagem> adapter;
 
+    // Sobrepoe o método onCreate, chama a primeira tela e alguns métodos.
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -37,6 +39,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         configuraLista();
     }
 
+    // Método para a configuração do botão "FloatingActionButton" que abre um formulário para adicionar um personagem.
     private void configuraFabNovoPersonagem(){
         FloatingActionButton botaoNovoPersonagem = findViewById(R.id.fab_add);
         botaoNovoPersonagem.setOnClickListener(new View.OnClickListener(){
@@ -45,26 +48,31 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         });
     }
 
+    // Método para abrir o formulário.
     private void abreFormulario(){
         startActivity(new Intent(this, FormularioPersonagemActivity.class));
     }
 
+    // Sobrepoe o método onResume da classe pai, chama o método onResume() e atualizaPersonagem().
     @Override
     protected void onResume(){
         super.onResume();
         atualizaPersonagem();
     }
 
+    // Método que atualiza e exibe cada personagem criado.
     private void atualizaPersonagem(){
         adapter.clear();
         adapter.addAll(dao.todos());
     }
 
+    // Método que remove o personagem e apaga ele da lista.
     private void remove(Personagem personagem){
         dao.remove(personagem);
         adapter.remove(personagem);
     }
 
+    // Sobrepoe o método onCreateContextMenu da classe pai infla o menu.
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
         super.onCreateContextMenu(menu, v, menuInfo);
@@ -72,6 +80,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.activity_lista_personagem_menu, menu);
     }
 
+    // Sobrepoe o método onContextItemSelected da classe pai e mostra opções quando um item for selecionado, como deseja remover? e a escolha do usuário (sim ou não).
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item){
 
@@ -94,6 +103,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         return super.onContextItemSelected(item);
     }
 
+    // Método que configura a lista, o visual e suas funcionalidades.
     private void configuraLista(){
         ListView listaDePersonagens = findViewById(R.id.activity_main_lista_personagem);
         configuraAdapter(listaDePersonagens);
@@ -101,6 +111,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         registerForContextMenu(listaDePersonagens);
     }
 
+    // Método que possibilita clicar em um personagem e editar ele.
     private void configuraItemPorClique(ListView listaDePersonagens){
         listaDePersonagens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -111,12 +122,14 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         });
     }
 
+    // Método qeu abre a edição do personagem.
     private void abreFormularioEditar(Personagem personagemEscolhido){
         Intent vaiParaFormulario = new Intent(ListaPersonagemActivity.this, FormularioPersonagemActivity.class);
         vaiParaFormulario.putExtra(CHAVE_PERSONAGEM, personagemEscolhido);
         startActivity(vaiParaFormulario);
     }
 
+    // Método que configura a lista com os personagens.
     private void configuraAdapter(ListView listaDePersonagens){
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         listaDePersonagens.setAdapter(adapter);
